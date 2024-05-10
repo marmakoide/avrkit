722c2868c295dbea4175be4035781ba987f63134
marmakoide <alexandre.devert@algokhod.com>
2024-05-10 18:18:34 +0200

    Added pin-trigger example

---
+++ examples/pin-trigger/arduino-uno/main.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#include <avrkit/GPIO.h>
+
+
+ISR(PCINT0_vect) {
+	if (gpio_pin_B4__is_high())
+	    gpio_pin_B5__set_low();
+	else
+	    gpio_pin_B5__set_high();
+}
+
+
+int
+main(void) {
+	// Trigger pin setup
+	gpio_pin_B4__set_as_input();
+	gpio_pin_B4__set_high();
+	gpio_pin_B4__enable_change_interrupt();
+
+	// Led setup
+	gpio_pin_B5__set_as_output();
+	gpio_pin_B5__set_low();
+	
+	// Main loop
+	sei();
+	while(1)
+		sleep_mode();
+}
