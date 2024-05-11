2b5feefa1af66a16cedabe4c2978fad56e475239
marmakoide <alexandre.devert@algokhod.com>
2024-05-11 22:14:43 +0200

    Removed duplication in examples Makefiles

---
+++ usb-settings.mk
@@ -0,0 +1,11 @@
+# Arduino Nano Chinese clone
+USB_PORT=/dev/ttyUSB0
+USB_BAUDS=57600
+
+# Arduino UNO
+#USB_PORT=/dev/ttyUSB0
+#USB_BAUDS=115200
+
+# Arduino Leonardo
+#USB_PORT=/dev/ttyACM0
+#USB_BAUDS=115200
